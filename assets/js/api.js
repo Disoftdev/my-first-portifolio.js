abf7b8ad36518ed27d341a522a2a384f91fddef7
Disoftdev <diego.personal007@gmail.com>
2025-04-08 21:26:03 +0100

    Atualiza api.js com as novas alterações

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Disoftdev/js-developer-portfolio/refs/heads/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Disoftdev/my-first-portifolio.js/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
